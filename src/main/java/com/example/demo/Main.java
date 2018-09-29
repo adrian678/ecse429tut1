8c4b873ce97f9a7ab1e161eb7bb1358adca5692e
Adrian-guindani <adrian.guindani@mail.mcgill.ca>
2018-09-29 07:38:15 -0400

    deleted original file with class method, left 2nd class w/ main method

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,8 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
